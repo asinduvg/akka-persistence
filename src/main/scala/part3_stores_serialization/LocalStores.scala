4421e6d9339b944d04674c4b23a28928bd1779b2
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2025-01-16 19:11:15 +0530

    PostgreSQL

--- src/main/scala/part3_stores_serialization/LocalStores.scala
+++ src/main/scala/part3_stores_serialization/LocalStores.scala
@@ -1,45 +1,9 @@
 package part3_stores_serialization
 
-import akka.persistence.{ PersistentActor, RecoveryCompleted, SaveSnapshotFailure, SaveSnapshotSuccess, SnapshotOffer }
-import akka.actor.{ ActorLogging, ActorSystem, Props }
+import akka.actor.{ActorSystem, Props}
 import com.typesafe.config.ConfigFactory
 
 object LocalStores extends App {
-
-  class SimplePersistentActor extends PersistentActor with ActorLogging {
-
-    override def persistenceId: String = "simple-persistent-actor"
-
-    // mutable state
-    var nMessages = 0
-
-    override def receiveCommand: Receive = {
-      case "print" =>
-        log.info(s"I have persisted $nMessages so far")
-      case "snap" =>
-        saveSnapshot(nMessages)
-      case SaveSnapshotSuccess(metadata) =>
-        log.info(s"Save snapshot was successful: $metadata")
-      case SaveSnapshotFailure(_, cause) =>
-        log.warning(s"Saving snapshot failed: $cause")
-      case message =>
-        persist(message) { _ =>
-          log.info(s"Persisting $message")
-          nMessages += 1
-        }
-    }
-
-    override def receiveRecover: Receive = {
-      case SnapshotOffer(_, payload: Int) =>
-        log.info(s"Recovered payload: $payload")
-        nMessages = payload
-      case RecoveryCompleted =>
-        log.info("Recovery done")
-      case message =>
-        log.info(s"Recovered: $message")
-        nMessages += 1
-    }
-  }
 
   val localStoresActorSystem = ActorSystem("localStoresSystem", ConfigFactory.load().getConfig("localStores"))
   val persistentActor        = localStoresActorSystem.actorOf(Props[SimplePersistentActor], "simplePersistentActor")
